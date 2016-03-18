0c8b6aba84efb5660bf49b38628027571f9511aa
Zach Ballard <zbginji@gmail.com>
2016-03-18 12:04:07 -0500

    basic structure and navbar class setup

--- TimeEntryLab/Program.cs
+++ TimeEntryLab/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,82 @@ namespace TimeEntryLab
 {
     class Program
     {
+        static readonly NavigationBar NavigationBar = new NavigationBar();
+
         static void Main(string[] args)
         {
+            bool exit = false;
+           
+            var allDevelopers = new List<Developer>();
 
+            Welcome();
+
+            while (!exit)
+            {
+                //Developer view (should be able to see all developers, see lists of each involvement) 
+                if (NavigationBar.ViewLevel == 0)
+                {
+                    drawDevelopers(allDevelopers);
+                    DecideNavigation();
+                }
+
+                //Client view (should be able to see all clients, see lists of each involvement)
+                if (NavigationBar.ViewLevel == 1)
+                {
+                    drawClients(allDevelopers);
+                    DecideNavigation();
+                }
+
+                //Projects view (should be able to see all projects, see lists of each involvement)
+                if (NavigationBar.ViewLevel == 2)
+                {
+                    drawProjects(allDevelopers);
+                    DecideNavigation();
+
+                }
+
+                //Hours Worked Report (print out a report on all hours in tasks)
+                if (NavigationBar.ViewLevel == 3)
+                {
+                    drawReport(allDevelopers);
+                    DecideNavigation();
+                }
+
+                if (NavigationBar.ViewLevel == 4)
+                {
+                    exit = true;
+                }
+            }
+        }
+
+        public static void DecideNavigation()
+        {
+
+        }
+
+        public static void drawReport(List<Developer> allDevelopers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void drawProjects(List<Developer> allDevelopers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void drawClients(List<Developer> allDevelopers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void drawDevelopers(List<Developer> allDevelopers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Welcome()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ TimeEntryLab/viewLevelBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace TimeEntryLab
+{
+    class NavigationBar
+    {
+        public int ViewLevel { get; set; }
+        public int Selection { get; set; }
+
+        public List<string> ViewHeaders { get; set; }
+        public List<string> UserOptions { get; set; }
+
+        public NavigationBar()
+        {
+            ViewLevel = 0;
+            Selection = 0;
+
+            ViewHeaders.Add("==== Developer Level ==============================");
+            ViewHeaders.Add("==== Client Level =================================");
+            ViewHeaders.Add("==== Project Level ================================");
+            ViewHeaders.Add("==== Hours Worked Report ==========================");
+
+            UserOptions.Add("(a)dd developer, (r)emove developer, list of a developer's (t)asks, list of a developer's (n)otes, (c)hange levels, (e)xit");
+            UserOptions.Add("(a)dd a client, (r)emove client, list of (p)rojects for client, add a (n)ew project for a client, (n)otes about client, (c)hange levels, (e)xit");
+            UserOptions.Add("(r)emove a project, View all (t)asks for a project, (n)otes about a project, (a)ll hours worked report, (h)ours worked on a project, (c)hange levels, (e)xit");
+        }
+    }
+}
